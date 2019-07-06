a39d04c35c030361e4ce5e53be2def783202da34
Markus Hitter <mah@jump-ing.de>
2019-07-06 13:36:27 +0200

    Make release 1.0.2.

--- blockpaymentlogo.php
+++ blockpaymentlogo.php
@@ -32,7 +32,7 @@ class BlockPaymentLogo extends Module
 	{
 		$this->name = 'blockpaymentlogo';
 		$this->tab = 'front_office_features';
-		$this->version = '1.0.1';
+		$this->version = '1.0.2';
 		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
